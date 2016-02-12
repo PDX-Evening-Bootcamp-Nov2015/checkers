abfa1ac5cf59c43d72a645ac6cf92f81d0c053f4
dev <charles.webster04@gmail.com>
2016-02-11 21:30:25 -0800

    update

--- checkers_app/static/checkers_app/register.js
+++ checkers_app/static/checkers_app/register.js
@@ -1,46 +1,34 @@
-// //windows onload function
-// //loads functions upon opening webpage
-// //adds event listener to the submit button
-// window.onload = function() {
-//   document.getElementById("submit").addEventListener("click", checkForm, false);
-// };
-//
-// function checkForm(event){
-//
-//
-//   // errorDisplay1.style.display = 'none';
-//   // errorDisplay2.style.display = 'none';
-//   // errorDisplay3.style.display = 'none';
-//   // errorDisplay4.style.display = 'none';
-//   var xpass1 = document.getElementById("password1").value;
-//   var xpass2 = document.getElementById("password2").value;
-//   document.getElementById("login").noValidate = true;
-//   if (xname === null || xname ===""){
-//     event.preventDefault();
-//     errorDisplay1.style.display = 'inline';
-//   }else if (xusername === null || xusername ===""){
-//     event.preventDefault();
-//     errorDisplay2.style.display = 'inline';
-//   }else if (xemail === null || xemail ===""){
-//     errorDisplay3.style.display = 'inline';
-//     event.preventDefault();
-//   }else if (testEmail(xemail) === false){
-//     errorDisplay4.style.display = 'inline';
-//     event.preventDefault();
-//   } else {
+//windows onload function
+//loads functions upon opening webpage
+//adds event listener to the submit button
+window.onload = function() {
+  document.getElementById("submit").addEventListener("click", checkForm, false);
+};
+console.log( document.getElementById("test").value)
+function checkForm(event){
+  var xpass1 = document.getElementById("password1").value;
+  var xpass2 = document.getElementById("password2").value;
+  document.getElementById("login").noValidate = true;
+  document.getElementById("submit").noValidate = true;
+  if (xpass1 === null || xpass1 ===""){
+    event.preventDefault();
+  }else if (xpass1 != xpass2){
+    event.preventDefault();
+  } else {
 // event.preventDefault();
 // sessionStorage.setItem('username', xusername);
 // redirect();
-//   }
+console.log("password good")
+  }
+}
+
+//regex that is called by the checkform function to check proper email format
+// function testEmail(xemail){
+//   var regemail = /\S+@\S+\.\S+/;
+//   return regemail.test(xemail);
 // }
-//
-// //regex that is called by the checkform function to check proper email format
-// // function testEmail(xemail){
-// //   var regemail = /\S+@\S+\.\S+/;
-// //   return regemail.test(xemail);
-// // }
-//
-// //function called after checkform function is complete to redirect to gallery
-// // function redirect(){
-// //     window.location.href = "gallery.html";
-// // }
+
+//function called after checkform function is complete to redirect to gallery
+// function redirect(){
+//     window.location.href = "gallery.html";
+// }
