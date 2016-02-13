e1fd579db8e9ed946732a207f19267a2fb0353db
dev <charles.webster04@gmail.com>
2016-02-12 19:43:28 -0800

    updated models.py

--- checkers_app/models.py
+++ checkers_app/models.py
@@ -4,22 +4,17 @@ from django.contrib.auth.models import User
 class Game(models.Model):
     open_session = models.BooleanField(default=False)
     black_player = models.ForeignKey(User, related_name="black_player")
-    red_player = models.ForeignKey(User)
+    red_player = models.ForeignKey(User, null=True)
     which_player = models.PositiveSmallIntegerField(default=0)
     jumped = models.BooleanField(default=False)
     turn_over = models.BooleanField(default=False)
 
 class Piece(models.Model):
-    king_Status = models.BooleanField(default=False)
+    king_status = models.BooleanField(default=False)
     color = models.CharField(max_length=10)
 
 class Space(models.Model):
     x_coordinate = models.PositiveSmallIntegerField(default=0)
     y_coordinate = models.PositiveSmallIntegerField(default=0)
-    piece_id = models.BooleanField(default=False)
-    black_player = models.ForeignKey(Piece)
-
-
-
-
-
+    piece_id = models.ForeignKey(Piece, null=True)
+    game_id = models.ForeignKey(Game)
