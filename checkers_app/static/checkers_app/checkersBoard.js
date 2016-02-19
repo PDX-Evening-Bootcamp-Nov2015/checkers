b7377345618187b6f71999b00b178ecb16bb97b4
pdxgitit <charles.webster04@gmail.com>
2016-02-18 21:33:31 -0800

    working spaces and pieces populating

--- checkers_app/static/checkers_app/checkersBoard.js
+++ checkers_app/static/checkers_app/checkersBoard.js
@@ -1,7 +1,5 @@
 //Functions assigned to buttons in the HTML
 window.onload = function() {
-console.log("test");
-
 document.getElementById('board').addEventListener('click', selectSquare);
 document.getElementById('getPositions').addEventListener('click', get_positions);
 gameUrl = window.location.pathname;
@@ -19,16 +17,41 @@ function selectSquare (event){
       error: function(result){console.log("Error")}
   });
 }
+
 function get_positions () {
 var response = $.ajax({
   type: 'GET',
   url: "/get_positions/" + gameUrl.split("/game/").pop(),
   contentType: "application/json",
-  success: function(result){console.log(result)},
+  success: function(result){display_pieces(result)},
   error: function(result){console.log("error")}
 
 });
 }
+
+function display_pieces(data) {
+var listOfPieces = data["key"];
+for (i=0;i<listOfPieces.length;i++){
+    if (listOfPieces[i][2] === "Red"){
+      var a = listOfPieces[i][0] + "" + listOfPieces[i][1];
+      var test = document.getElementById(String(a));
+      test.children[0].src = redPieceNonKing;
+    } else if (listOfPieces[i][2] === "Black") {
+      var a = listOfPieces[i][0] + "" + listOfPieces[i][1];
+      var test = document.getElementById(String(a));
+      test.children[0].src = blackPieceNonKing;
+
+
+    }
+
+
+
+  // if ("" + listOfPieces[i][0] + "" + listOfPieces[i][1] === )
+
+}
+
+}
+
 gameUrl = window.location.pathname;
 console.log(gameUrl.split("/game/").pop());
 };
